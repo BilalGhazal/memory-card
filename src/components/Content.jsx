d3a27648d4c148333898e72ae810ef4714a6b89e
Bilal Ghazal <bilalghazal98@gmail.com>
2025-05-29 16:52:01 +0300

    Make characters display in random order

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,17 +1,26 @@
+import { useRef } from "react"
 import "../styles/content.css"
 
 
 export default function Content({characters, onClick}) {
+    
+    function shuffleCharacters() {
+        const shuffledCharacters = [...characters].sort(() => Math.random() - 0.5)
+
+        return shuffledCharacters.map(dataObj => {
+            return (
+                <div className="character" key={dataObj.id} onClick={() => onClick(dataObj.id)}>
+                    <img src={dataObj.image} />
+                    <p>{dataObj.name}</p>
+                </div>
+            )
+        })
+    }
+
+
     return (
         <div className="content">
-            {characters.map(dataObj => {
-                return (
-                    <div className="character" key={dataObj.id} onClick={() => onClick(dataObj.id)}>
-                        <img src={dataObj.image} />
-                        <p>{dataObj.name}</p>
-                    </div>
-                )
-            })}
+           {shuffleCharacters()}
         </div>
     )
 }
